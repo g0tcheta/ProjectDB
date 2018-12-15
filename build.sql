69dce889f62331d7f5e8c2a2af14ca8c7da71e93
unknown <julienrichard98@gmail.com>
2018-12-15 15:06:33 +0100

    MaJ

--- build.sql
+++ build.sql
@@ -1,5 +1,6 @@
--- DROP
 
+
+-- DROP
 
 
 DROP TABLE IF EXISTS Athlete CASCADE;
@@ -10,9 +11,7 @@ DROP TABLE IF EXISTS Resultat CASCADE;
 DROP TABLE IF EXISTS Date CASCADE;
 
 
-
 -- CREATION
-
 
 
 CREATE TABLE Athlete
@@ -20,10 +19,10 @@ CREATE TABLE Athlete
   id_athlete SERIAL PRIMARY KEY,
   id_sport INT,
   FOREIGN KEY (id_sport) REFERENCES Sport(id_sport),
-  nom VARCHAR(100),
-  genre VARCHAR(100),
+  nom VARCHAR(255),
+  genre VARCHAR(255),
   age INT,
-  pays VARCHAR(100)
+  pays VARCHAR(255)
 );
 
 CREATE TABLE Team
@@ -31,12 +30,12 @@ CREATE TABLE Team
   id_team SERIAL PRIMARY KEY,
   id_sport INT,
   FOREIGN KEY (id_sport) REFERENCES Sport(id_sport),
-  nom VARCHAR(100),
-  genre VARCHAR(100),
-  pays VARCHAR(100),
+  nom VARCHAR(255),
+  genre VARCHAR(255),
+  pays VARCHAR(255),
 )
 
-CREATE TABLE MembreDe
+CREATE TABLE Membre
 (
   id_athlete int,
   FOREIGN KEY (id_athlete) REFERENCES Athlete(id_athlete),
@@ -45,25 +44,32 @@ CREATE TABLE MembreDe
   PRIMARY KEY (id_athlete, id_team)
 )
 
+CREATE TABLE Participant
+(
+  id_particpant SERIAL PRIMARY KEY,
+  id_athlete int,
+  FOREIGN KEY (id_athlete) REFERENCES Athlete(id_athlete),
+  id_team int,
+  FOREIGN KEY (id_team) REFERENCES Team(id_team),
+);
 
 CREATE TABLE Sport
 (
   id_sport SERIAL PRIMARY KEY,
-  nom VARCHAR(100),
-  categorie VARCHAR(100),
-  multi VARCHAR(100)
+  nom VARCHAR(255),
+  categorie VARCHAR(255),
+  multi VARCHAR(255)
 );
 
 CREATE TABLE Event
-  (
-    id_event SERIAL PRIMARY KEY,
-    id_sport INT;
-    FOREIGN KEY (id_sport) REFERENCES Sport(id_sport),
-    id_particpant INT;
-    FOREIGN KEY (id_particpant) REFERENCES Athlete(id_athlete) AND Team (id_team),
-    date DATE
-  );
-
+(
+  id_event SERIAL PRIMARY KEY,
+  id_sport INT;
+  FOREIGN KEY (id_sport) REFERENCES Sport(id_sport),
+  id_particpant INT;
+  FOREIGN KEY (id_particpant) REFERENCES ,
+  date DATE
+);
 
 CREATE TABLE Resultat
 (
@@ -73,30 +79,46 @@ CREATE TABLE Resultat
   id_event INT,
   FOREIGN KEY (id_event) REFERENCES Event(id_event),
   temps TIME,
-  score INT,
+  score VARCHAR(11),
 );
 
 CREATE TABLE Medaille
 (
   id_medaille SERIAL PRIMARY KEY,
-  type VARCHAR(100)
+  type VARCHAR(255)
 );
 
 
-
 -- INSERTION
+
 
 INSERT INTO Athlete (id_sport,nom,genre,age,pays) VALUES
 (''),
 ('');
 
+INSERT INTO Team (id_sport,nom,genre,pays) VALUES
+(,'','',''),
+(,'','','');
+
+INSERT INTO Membre (id_athlete, id_team) VALUES
+(,),
+(,);
+
+INSERT INTO Participant (id_athlete, id_team) VALUES
+(,),
+(,);
+
 INSERT INTO Sport (nom, categorie, multi) VALUES
-(''),
-('');
+('','',''),
+('','','');
+
+INSERT INTO Event (id_sport, id_particpant, date) VALUES
+(,,DATE '2015-12-17'),
+(,,DATE '2015-12-17');
 
 INSERT INTO Resultat (id_medaille, id_event, temps, score) VALUES
-(''),
-('');
+(,,'00:00',''),
+(,,'00:00','');
 
 INSERT INTO Medaille (type) VALUES
 ('NUL'),
