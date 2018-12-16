d9c8b05a6c540f5d00898f54ef3d47ddcfacb8d4
unknown <julienrichard98@gmail.com>
2018-12-16 20:00:00 +0100

    aa

--- requetes.sql
+++ requetes.sql
@@ -1,6 +1,6 @@
 SELECT DISTINCT athlete.nom
 FROM athlete , event , resultat
-WHERE id_medaille != 0 
+WHERE id_medaille != 0
 AND pays = 'Italie'
 
 SELECT DISTINCT athlete.nom,
@@ -12,9 +12,9 @@ WHERE athlete.id_sport = 1
 OR athlete.id_sport = 2
 OR athlete.id_sport = 3
 
-SELECT DISTINCT athlete.nom 
+SELECT DISTINCT athlete.nom
 FROM athlete ,team
-WHERE athlete.genre = 'F' 
+WHERE athlete.genre = 'F'
 AND athlete.id_sport = 24
 AND age < 25
 
@@ -26,23 +26,20 @@ FROM sport JOIN event ON sport.id_sport = event.id_sport
 JOIN resultat ON event.id_event = resultat.id_event
 JOIN participant ON event.id_participant = participant.id_participant
 JOIN athlete ON participant.id_athlete = athlete.id_athlete
-WHERE athlete.nom  = 'Michael Phelps' 
+WHERE athlete.nom  = 'Michael Phelps'
 
 SELECT nom
 FROM sport
 WHERE multi = 'multi'
 
 SELECT MIN(temps) as record_marathon
-FROM resultat JOIN event ON resultat.id_event = event.id_event 
+FROM resultat JOIN event ON resultat.id_event = event.id_event
 WHERE event.id_sport = 46
 
 SELECT AVG(temps) as moyenne_temps,
 pays
 FROM resultat JOIN event ON resultat.id_event = event.id_event
-JOIN participant ON event.id_participant = participant.id_participant 
+JOIN participant ON event.id_participant = participant.id_participant
 JOIN athlete ON participant.id_athlete = athlete.id_athlete
 WHERE athlete.id_sport = 28
 GROUP BY pays
-
-
-
