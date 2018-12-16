697de364399d7282ce37141c16faca785d5848d1
unknown <julienrichard98@gmail.com>
2018-12-16 18:57:17 +0100

    a

--- requetes.sql
+++ requetes.sql
@@ -1,7 +1,5 @@
-SELECT COUNT(Medaille.idmedaille), Athlete.Pays
-FROM Medaille, Resultat, Event, Participation, Athlete
-WHERE Medaille.IdMedaille = Resultat.Idrésulat
-AND Resulat.ideven = event.idevent
-AND Event.idparticipant = Participation.id_participant
-AND Participation.IdAthlete = Athlete.Idathlete
-GROUP BY Athlete.Pays;
+SELECT COUNT(resultat.id_medaille),
+athlete.pays
+FROM resultat JOIN event on resultat.id_event = event.id_event
+JOIN athlete ON event.id_sport = athlete.id_sport
+GROUP BY athlete.pays
