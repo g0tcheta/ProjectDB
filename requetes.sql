a48f8aca06635232c2cca151b84d4365d3885a9e
unknown <julienrichard98@gmail.com>
2018-12-16 18:52:10 +0100

    a

--- requetes.sql
+++ requetes.sql
@@ -1,40 +1,7 @@
-1*
-
-1)
-Select gagnant 
-from Resultat
-
-2)
-Select nom , id_pays , medaille
-from Athlète join Resultat on nom = gagnant
-where epreuve = '100m' and epreuve = '200m' and epreuve = '400m'
-order by medaille
-
-3)
-select nom , prenom 
-from Athlète join epreuve on discipline = epreuve.nom
-where epreuve.nom = 'handball' and age < 25 and genre = 'femme'
-
-4)
-Select médaille , score , epreuve
-from resultat join athlète on nom = gagnant 
-where nom = 'Michael Phelps'
-
-5)
-select nom 
-from epreuve 
-where indiv/equipê = 'equipe'
-
-6)
-select max(score)
-from resusltat 
-where epreuve ='marathon'
-
-
-2*
-
-1)
-select AVG(score) , id_pays
-from resultat join athlète on gagnant = nom
-where epreuve = '200m nage libre'
-order by id_pays
+SELECT COUNT(Medaille.idmedaille), Athlete.Pays
+FROM Medaille, Resultat, Event, Participation, Athlete
+WHERE Medaille.IdMedaille = Resultat.Idrésulat
+AND Resulat.ideven = event.idevent
+AND Event.idparticipant = Participation.id_participant
+AND Participation.IdAthlete = Athlete.Idathlete
+GROUP BY Athlete.Pays;
